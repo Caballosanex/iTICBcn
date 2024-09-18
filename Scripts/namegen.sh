5fd9e694a4167593b4d6ad65b56c311b2556faf2
Àlex Sánchez Blabia <alexsanchezblabia@gmail.com>
2024-09-18 13:55:00 +0200

    Tests passed

--- Scripts/namegen.sh
+++ Scripts/namegen.sh
@@ -5,8 +5,8 @@
 #                                                     +:+ +:+         +:+      #
 #    By: alexsanc <2024_alex.sanchez@iticbcn.cat    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2024/09/16 20:34:59 by alexsanc          #+#    #+#              #
-#    Updated: 2024/09/18 12:43:58 by alexsanc         ###   ########.fr        #
+#    Created: 2024/09/18 13:53:41 by alexsanc          #+#    #+#              #
+#    Updated: 2024/09/18 13:53:42 by alexsanc         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -23,6 +23,9 @@ else
   read route
 fi
 
+# Debug: print the file route
+echo "Route provided: $route"
+
 # Check if the file exists
 if [ ! -f "$route" ]; then
   echo "Error: File does not exist."
@@ -37,6 +40,10 @@ if [[ "$name" == *.* ]]; then
 else
    extension=""
 fi
+
+# Debug: print file details
+echo "File name: $name"
+echo "File extension: $extension"
 
 # The script must now prompt the user to choose the subject related to the file.
 while true; do
@@ -56,20 +63,73 @@ while true; do
     fi
 done
 
-# Get the current date and time and store it in the variable "date".
-date=$(date +"%Y%m%d_%H%M%S")
-
-# Define the new filename based on the subject and date, adding special behavior for GBD.
+# Define the new filename based on the subject, adding special behavior for GBD.
 newname=""
 case $subject in
-    1)
-        newname="FM_Sost_$date"
-        ;;
-    2)
-        newname="TUT_$date"
+    1|2|4|5|6)
+        # Ask for RA (Resultat Avaluació)
+        while true; do
+            echo "Please enter the Resultat Avaluació number (e.g., 1 for RA1, 2 for RA2):"
+            read RA
+
+            if [[ "$RA" =~ ^[1-9][0-9]*$ ]]; then
+                RA="RA$RA"
+                break
+            else
+                echo "Invalid input. Please enter a valid number for RA."
+            fi
+        done
+
+        # Ask for the Activity (AC) number
+        while true; do
+            echo "Please enter the Activity number (e.g., 1 for AC01, 2 for AC02):"
+            read ac
+
+            if [[ "$ac" =~ ^[1-9][0-9]*$ ]]; then
+                ac=$(printf "AC%02d" "$ac")
+                break
+            else
+                echo "Invalid input. Please enter a valid number for Activity."
+            fi
+        done
+
+        # Ask for the last name and first name
+        echo "Please enter your last name:"
+        read lastname
+
+        echo "Please enter your first name:"
+        read firstname
+
+        # Check for invalid characters in the last name and first name
+        if [[ "$lastname" =~ [^a-zA-Z] || "$firstname" =~ [^a-zA-Z] ]]; then
+            echo "Error: Names contain invalid characters. Only alphabetic characters are allowed."
+            exit 1
+        fi
+
+        # Define subject names
+        case $subject in
+            1)
+                subject_name="FM_Sost"
+                ;;
+            2)
+                subject_name="TUT"
+                ;;
+            4)
+                subject_name="ISO"
+                ;;
+            5)
+                subject_name="PAX"
+                ;;
+            6)
+                subject_name="LMSGI"
+                ;;
+        esac
+
+        # Create the new name based on the subject format
+        newname="${subject_name}_${RA}_${ac}_${lastname}_${firstname}"
         ;;
     3)
-        # Ask for RA (Resultat Avaluació)
+        # Special handling for GBD
         while true; do
             echo "Please enter the Resultat Avaluació number (e.g., 1 for RA1, 2 for RA2):"
             read RA
@@ -111,15 +171,6 @@ case $subject in
         # Create the new name based on the GBD/ASGBD format
         newname="GBD_${RA}_${ac}_${lastname}_${firstname}"
         ;;
-    4)
-        newname="ISO_$date"
-        ;;
-    5)
-        newname="PAX_$date"
-        ;;
-    6)
-        newname="LMSGI_$date"
-        ;;
     *)
         echo "Invalid option."
         exit 1
@@ -144,6 +195,8 @@ fi
 
 # Check if a file with the same name already exists
 target_path="$(dirname "$route")/$final_name"
+echo "Target path: $target_path"
+
 if [ -f "$target_path" ]; then
     echo "Warning: A file named $final_name already exists."
     echo "Do you want to overwrite it? (y/n)"
